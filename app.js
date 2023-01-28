af24cb8bb9b4b7537193178382c16c2777984d62
usamauzair-web <usama19026@gmail.com>
2023-01-28 07:21:23 +0530

    backend done

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+require("./models/user");
+require("./models/post");
+
+app.use(express.json());
+app.use(require("./routes/auth"));
+app.use(require("./routes/post"));
+app.use(require("./routes/user"));
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 mongoose.connection.on("connected", () => {
   console.log("conneted to mongo yeahh");
 });
