e6968aca438beabc1ca5b5cb90ddea6f5664ce7a
usamauzair-web <usama19026@gmail.com>
2023-01-30 09:40:49 +0530

    SignUp modified

--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -47,39 +47,46 @@ const SignIn = () => {
       });
   };
   return (
-    <div style={{ display: "block" }}>
-      <div className="mycard">
-        <div className="card auth-card input-field">
-          <h2>SurgeInsta</h2>
-          <input
-            type="text"
-            placeholder="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="password"
-            value={password}
-            onChange={(e) => setPasword(e.target.value)}
-          />
-          <button
-            className="btn waves-effect waves-light #64b5f6 blue darken-1"
-            onClick={() => PostData()}
-          >
-            Login
-          </button>
-          <h5>
-            <Link to="/signup">Dont have an account ?</Link>
-          </h5>
-          <h6>
-            <Link to="/reset">Forgot password ?</Link>
-          </h6>
+    <div className="row">
+      <div className="col s6">
+        <div className="mycard">
+          <div className="card auth-card input-field">
+            <h2>SurgeInsta</h2>
+            <input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPasword(e.target.value)}
+            />
+            <button
+              className="btn waves-effect waves-light #64b5f6 blue darken-1"
+              onClick={() => PostData()}
+            >
+              Login
+            </button>
+            <h5>
+              <Link to="/signup">Dont have an account ?</Link>
+            </h5>
+            <h6>
+              <Link to="/reset">Forgot password ?</Link>
+            </h6>
+          </div>
         </div>
       </div>
-      <div>
-        <h3>Uzama Uzair</h3>
-        <h2>Software Engineering Intern March 2023</h2>
+
+      <div className="col s6">
+        <div className="mycard">
+          <div className="card auth-card input-field">
+            <h2>Surge SE Intern March 2023</h2>
+            <h3>Uzama Uzair</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
